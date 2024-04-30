0d98825a69e6b24ab33361d26979d8268491b633
apoorvar5 <apoorvrumale@gmail.com>
2024-04-29 17:48:23 -0700

    update till login functionality

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { PlayersComponent } from './players/players.component';
 import { ClubsComponent } from './clubs/clubs.component';
 import { ClubPlayersComponent } from './clubs/club-players.component';
 import { PlayerClubsComponent } from './players/player-clubs.component';
+import { LoginComponent } from './auth/login.component';
 
 export const routes: Routes = [
     {path : '', component : HelloComponent, pathMatch : 'full'},
     {path : 'players', component : PlayersComponent},
     {path : 'clubs', component : ClubsComponent},
     {path: 'clubplayers/:id', component : ClubPlayersComponent},
-    {path: 'playerclubs/:id', component: PlayerClubsComponent}
+    {path: 'playerclubs/:id', component: PlayerClubsComponent},
+    {path: 'login', component: LoginComponent},
 ];
